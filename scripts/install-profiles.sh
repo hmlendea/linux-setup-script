ac2d65ab77ac92a321edb4bcb06badf347cb0012
Mlendea, Horațiu <Mlendea.Horatiu@GMail.com>
2019-08-29 20:48:27 +0300

    Profiles installation

---
+++ scripts/install-profiles.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+SYSTEM_PROFILES_DIRECTORY_PATH="/etc/profile.d"
+
+for PROFILE_FILE_NAME in $(ls "profiles"); do
+    PROFILE_SOURCE_FILE_PATH="${PWD}/profiles/${PROFILE_FILE_NAME}"
+    PROFILE_TARGET_FILE_PATH="${SYSTEM_PROFILES_DIRECTORY_PATH}/${PROFILE_FILE_NAME}"
+    echo " > Installing profile '${PROFILE_FILE_NAME}'"
+
+    cp -f "${PROFILE_SOURCE_FILE_PATH}" "${PROFILE_TARGET_FILE_PATH}"
+    chmod +x "${PROFILE_TARGET_FILE_PATH}"
+done
+
