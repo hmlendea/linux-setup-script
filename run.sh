e426eedc141dd2affcbf49839bbdb00d65618eeb
Mlendea, Horațiu <Mlendea.Horatiu@GMail.com>
2020-01-18 16:54:44 +0200

    Fix su rights

--- run.sh
+++ run.sh
@@ -70,8 +70,8 @@ update-system
 remove-unused-dependencies
 
 execute-script "config-system.sh"
-execute-script "set-system-locale-timedate.sh"
 
+execute-script-superuser "set-system-locale-timedate.sh"
 execute-script-superuser "install-profiles.sh"
 execute-script-superuser "customise-launchers.sh"
 
