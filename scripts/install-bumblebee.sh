90a9fca6bfe6aba7106e52c0b7ef9983c775a185
Mlendea, Horațiu <Mlendea.Horatiu@GMail.com>
2021-01-25 18:57:57 +0200

    Bumblebee installation script

---
+++ scripts/install-bumblebee.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+SCRIPT_PATH=$(realpath $0)
+SCRIPT_DIR_PATH=$(dirname ${SCRIPT_PATH})
+cd ${SCRIPT_DIR_PATH}
+source "${SCRIPT_DIR_PATH}/package-manager-functions.sh"
+
+NVIDIA_DRIVER="nvidia-390xx"
+
+install-pkg bumblebee
+install-dep bbswitch
+
+install-pkg mesa
+install-pkg xf86-video-intel
+
+install-pkg ${NVIDIA_DRIVER}-dkms
+install-pkg ${NVIDIA_DRIVER}-settings
+install-dep lib32-${NVIDIA_DRIVER}-utils
+
+install-dep lib32-virtualgl
