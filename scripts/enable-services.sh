f54cb784b5f033755ba5f4bf670e85fd1a143154
Mlendea, Horațiu <io.github.hmlendea.cnzq9@SimpleLogin.fr>
2021-02-06 14:59:29 +0200

    Enable the NM service

--- scripts/enable-services.sh
+++ scripts/enable-services.sh
@@ -6,6 +6,7 @@ function enable-service {
 }
 
 [ -f "/usr/bin/thermald" ]                              && enable-service "thermald.service"
+[ -f "/usr/bin/networkctl" ]                            && enable-service "NetworkManager.service"
 [ -f "/usr/bin/ntpd" ]                                  && enable-service "ntpd.service"
 [ -f "/usr/lib/systemd/system/yaourt-auto-sync.timer" ] && enable-service "yaourt-auto-sync.timer"
 [ -f "/lib/systemd/system/sshd.service" ]               && enable-service "sshd.service"
