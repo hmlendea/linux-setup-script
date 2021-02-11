d54aeda4e3a3875d75663e927dbcc9422fbe1a73
Mlendea, Horațiu <Mlendea.Horatiu@GMail.com>
2021-02-11 17:33:42 +0200

    Reorganised the shell RC

--- scripts/update-rcs.sh
+++ scripts/update-rcs.sh
@@ -15,7 +15,11 @@ copy_rc() {
     cp "$RC_DIR/$SOURCE_RC" "$TARGET_RC"
 }
 
-copy_rc "bashrc" "$HOME/.bashrc"
+copy_rc "shell_aliases" "${HOME}/.shell_aliases"
+copy_rc "shell_vars" "${HOME}/.shell_vars"
+
+[ -f "/bin/bash" ] && copy_rc "bashrc" "${HOME}/.bashrc"
+[ -f "/bin/bash" ] && copy_rc "bashrc" "${HOME}/.bash_prompt"
 
 [ -f "/usr/bin/nano" ]      && copy_rc "nanorc"     "${HOME}/.nanorc"
 [ -f "/usr/bin/vim" ]       && copy_rc "vimrc"      "${HOME}/.vimrc"
