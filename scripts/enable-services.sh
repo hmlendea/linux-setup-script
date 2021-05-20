4645c3a47b293bd3774c6d7dd6ebf2860cd10522
Mlendea, Horațiu <Horatiu.Mlendea@ProtonMail.com>
2021-05-20 19:57:47 +0300

    Enable service systemd-timesyncd

--- scripts/enable-services.sh
+++ scripts/enable-services.sh
@@ -5,9 +5,10 @@ function enable-service {
     systemctl start $1
 }
 
-[ -f "/usr/bin/thermald" ]                              && enable-service "thermald.service"
-[ -f "/usr/bin/networkctl" ]                            && enable-service "NetworkManager.service"
-[ -f "/usr/bin/ntpd" ]                                  && enable-service "ntpd.service"
-[ -f "/usr/lib/systemd/system/yaourt-auto-sync.timer" ] && enable-service "yaourt-auto-sync.timer"
-[ -f "/lib/systemd/system/sshd.service" ]               && enable-service "sshd.service"
-[ -f "/usr/bin/blueman" ]                               && enable-service "bluetooth.service"
+[ -f "/usr/bin/thermald" ]                                  && enable-service "thermald.service"
+[ -f "/usr/bin/networkctl" ]                                && enable-service "NetworkManager.service"
+[ -f "/usr/bin/ntpd" ]                                      && enable-service "ntpd.service"
+[ -f "/usr/lib/systemd/system/systemd-timesyncd.service" ]  && enable-service "systemd-timesyncd.service"
+[ -f "/usr/lib/systemd/system/yaourt-auto-sync.timer" ]     && enable-service "yaourt-auto-sync.timer"
+[ -f "/lib/systemd/system/sshd.service" ]                   && enable-service "sshd.service"
+[ -f "/usr/bin/blueman" ]                                   && enable-service "bluetooth.service"
