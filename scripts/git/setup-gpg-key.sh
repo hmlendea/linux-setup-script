ed25869fe60582e22c7845d1b8df5925b2965b94
Mlendea, Horațiu <Horatiu.Mlendea@ProtonMail.com>
2021-11-09 15:59:39 +0200

    Code quality improvements

--- scripts/git/setup-gpg-key.sh
+++ scripts/git/setup-gpg-key.sh
@@ -4,16 +4,16 @@ PUBRING_FILE_PATH="${HOME}/.gnupg/pubring.kbx"
 GITHUB_KEY_ID_FILE_PATH="${HOME}/.gnupg/github_key_id.txt"
 GITCONFIG_FILE_PATH="${HOME}/.gitconfig"
 
-if [ -f ${PUBRING_FILE_PATH} ] &&
-   [ -f ${GITHUB_KEY_ID_FILE_PATH} ]; then
-    GITHUB_GPG_KEY_ID=$(cat ${GITHUB_KEY_ID_FILE_PATH})
+if [ -f "${PUBRING_FILE_PATH}" ] &&
+   [ -f "${GITHUB_KEY_ID_FILE_PATH}" ]; then
+    GITHUB_GPG_KEY_ID=$(<"${GITHUB_KEY_ID_FILE_PATH}")
 
-    echo "Found GitHub GPG Key: ${GITHUB_GPG_KEY_ID}"
+    if [ -n "${GITHUB_GPG_KEY_ID}" ]; then
+        echo "Found GitHub GPG Key: ${GITHUB_GPG_KEY_ID}"
 
-    if [ ! -z "${GITHUB_GPG_KEY_ID}" ]; then
-        git config --global user.signingkey ${GITHUB_GPG_KEY_ID}
+        git config --global user.signingkey "${GITHUB_GPG_KEY_ID}"
         git config --global commit.gpgsign true
 
-        sed -i 's/\t/    /g' ${GITCONFIG_FILE_PATH}
+        sed -i 's/\t/    /g' "${GITCONFIG_FILE_PATH}"
     fi
 fi
