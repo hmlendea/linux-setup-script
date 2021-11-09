992d58b8918ac3f52b9e4f0bf55695054d8f3b3d
Mlendea, Horațiu <Horatiu.Mlendea@ProtonMail.com>
2021-11-09 16:34:42 +0200

    Moved some RCs to Resources

--- scripts/update-resources.sh
+++ scripts/update-resources.sh
@@ -45,3 +45,27 @@ copy_res "${ROOT_USR_BIN}/lxpanel" "plank/autostart.desktop"                "${H
 # PCManFM's context menu
 copy_res "${ROOT_USR_BIN}/code-oss"     "pcmanfm/open-in-code.desktop"      "${HOME}/.local/share/file-manager/actions/open-in-code.desktop"
 copy_res "${ROOT_USR_BIN}/lxterminal"   "pcmanfm/open-in-terminal.desktop"  "${HOME}/.local/share/file-manager/actions/open-in-terminal.desktop"
+
+if does-bin-exist "firefox"; then
+    FIREFOX_PROFILES_DIR="${HOME_REAL}/.mozilla/firefox"
+    FIREFOX_PROFILES_INI_FILE="${FIREFOX_PROFILES_DIR}/profiles.ini"
+
+    if [ -f "${FIREFOX_PROFILES_INI_FILE}" ]; then
+        FIREFOX_PROFILE_ID=$(grep "^Path=" "${FIREFOX_PROFILES_INI_FILE}" | awk -F= '{print $2}' | head -n 1)
+
+        update-file-if-needed "${REPO_RES_DIR}/firefox/userChrome.css" "${FIREFOX_PROFILES_DIR}/${FIREFOX_PROFILE_ID}/chrome/userChrome.css"
+    fi
+fi
+
+if does-bin-exist "neofetch"; then
+    NEOFETCH_CONFIG_DIR="${HOME_REAL}/.config/neofetch"
+    NEOFETCH_ASCII_LOGO_FILE=""
+
+    if [ "${DISTRO}" == "Arch Linux" ]; then
+        NEOFETCH_ASCII_LOGO_FILE="${REPO_RES_DIR}/neofetch-arch-ascii"
+    elif [ "${DISTRO}" == "LineageOS" ]; then
+        NEOFETCH_ASCII_LOGO_FILE="${REPO_RES_DIR}/neofetch-lineageos-ascii"
+    fi
+
+    [ -f "${NEOFETCH_ASCII_LOGO_FILE}" ] && update-file-if-needed "${NEOFETCH_ASCII_LOGO_FILE}" "${NEOFETCH_CONFIG_DIR}/neofetch-distro-ascii"
+fi
