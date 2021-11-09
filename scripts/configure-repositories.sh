ee6017923c6a440bf6f808256f377fb46588f992
Mlendea, Horațiu <io.github.hmlendea.cnzq9@SimpleLogin.fr>
2021-11-09 17:17:50 +0200

    Fixed adding new arch repos

--- scripts/configure-repositories.sh
+++ scripts/configure-repositories.sh
@@ -21,9 +21,9 @@ function add_repository {
         file-append-line "${PACMAN_CONF_FILE_PATH}" ""
         file-append-line "${PACMAN_CONF_FILE_PATH}" "[${NAME}]"
 
-        [ -n "${SERVER}" ]    && append_to_conf "Server = ${SERVER}\n"
-        [ -n "${INCLUDE}" ]   && append_to_conf "Include = ${INCLUDE}\n"
-        [ -n "${SIGLEVEL}" ]  && append_to_conf "SigLevel = ${SIGLEVEL}\n"
+        [ -n "${SERVER}" ]    && file-append-line "${PACMAN_CONF_FILE_PATH}" "Server = ${SERVER}"
+        [ -n "${INCLUDE}" ]   && file-append-line "${PACMAN_CONF_FILE_PATH}" "Include = ${INCLUDE}"
+        [ -n "${SIGLEVEL}" ]  && file-append-line "${PACMAN_CONF_FILE_PATH}" "SigLevel = ${SIGLEVEL}"
 
         if [ -n "${KEY}" ]; then
             pacman-key --recv-keys "${KEY}"
