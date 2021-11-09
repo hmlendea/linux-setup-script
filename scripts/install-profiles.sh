4d78bb5ecadadac4da282f0933454b4807c4a921
Mlendea, Horațiu <io.github.hmlendea.cnzq9@SimpleLogin.fr>
2021-11-09 16:30:09 +0200

    Simplified and improved profile install script

--- scripts/install-profiles.sh
+++ scripts/install-profiles.sh
@@ -1,15 +1,6 @@
 #!/bin/bash
 source "scripts/common/common.sh"
 
-[[ "${ARCH}" != "x86_64" ]] && exit
-
 SYSTEM_PROFILES_DIRECTORY_PATH="${ROOT_ETC}/profile.d"
 
-for PROFILE_FILE_RELATIVE_PATH in "profiles"/*; do
-    PROFILE_FILE_NAME=$(basename "${PROFILE_FILE_RELATIVE_PATH}")
-    PROFILE_SOURCE_FILE_PATH="${PWD}/${PROFILE_FILE_RELATIVE_PATH}"
-    PROFILE_TARGET_FILE_PATH="${SYSTEM_PROFILES_DIRECTORY_PATH}/${PROFILE_FILE_NAME}"
-
-    update-file-if-needed "${PROFILE_SOURCE_FILE_PATH}" "${PROFILE_TARGET_FILE_PATH}"
-    chmod +x "${PROFILE_TARGET_FILE_PATH}"
-done
+does-bin-exist "dotnet" && update-file-if-needed "profiles/dotnet.sh" "${SYSTEM_PROFILES_DIRECTORY_PATH}/dotnet.sh"
