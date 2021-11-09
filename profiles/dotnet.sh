115e2d1f31aa22994759d3eb640f0ec4aa265526
Mlendea, Horațiu <io.github.hmlendea.cnzq9@SimpleLogin.fr>
2021-11-09 16:27:31 +0200

    Updated the dotnet.sh profile

--- profiles/dotnet.sh
+++ profiles/dotnet.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-export DOTNET_ROOT=/usr/share/dotnet
-export DOTNET_VERSION=$("${DOTNET_ROOT}/dotnet" --version)
+export DOTNET_ROOT="/usr/share/dotnet"
+DOTNET_VERSION="$(dotnet --version)"
+
 export MSBuildSDKsPath="${DOTNET_ROOT}/sdk/${DOTNET_VERSION}/Sdks"
-export PATH="${PATH}:${DOTNET_ROOT}"
+
+[[ "${PATH}" != *"${DOTNET_TOOLS_PATH}"* ]] && export PATH="${PATH}:${DOTNET_TOOLS_PATH}"
+
+# Extract self-contained executables under HOME to avoid multi-user issues from using the default '/var/tmp'
+[ -z "${DOTNET_BUNDLE_EXTRACT_BASE_DIR}" ] && export DOTNET_BUNDLE_EXTRACT_BASE_DIR="${XDG_CACHE_HOME:-"${HOME}"/.cache}/dotnet_bundle_extract"
