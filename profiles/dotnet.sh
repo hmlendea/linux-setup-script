df51ce46d328d599696401db54fc49594b53b10b
Mlendea, Horațiu <Horatiu.Mlendea@ProtonMail.com>
2021-11-10 11:20:43 +0200

    Updated dotnet.sh profile

--- profiles/dotnet.sh
+++ profiles/dotnet.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
-export DOTNET_ROOT="/usr/share/dotnet"
+[ -z "${DOTNET_ROOT}" ] && [ -d "/usr/share/dotnet" ] && export DOTNET_ROOT="/usr/share/dotnet"
+
 DOTNET_VERSION="$(dotnet --version)"
 
-export MSBuildSDKsPath="${DOTNET_ROOT}/sdk/${DOTNET_VERSION}/Sdks"
+[ -z "${MSBuildSDKsPath}" ] && export MSBuildSDKsPath="${DOTNET_ROOT}/sdk/${DOTNET_VERSION}/Sdks"
 
-[[ "${PATH}" != *"${DOTNET_TOOLS_PATH}"* ]] && export PATH="${PATH}:${DOTNET_TOOLS_PATH}"
+# Add dotnet tools directory to PATH
+DOTNET_TOOLS_PATH="${HOME}/.dotnet/tools"
+case "${PATH}" in
+    *"${DOTNET_TOOLS_PATH}"* ) true ;;
+    * ) PATH="${PATH}:${DOTNET_TOOLS_PATH}" ;;
+esac
 
 # Extract self-contained executables under HOME to avoid multi-user issues from using the default '/var/tmp'
 [ -z "${DOTNET_BUNDLE_EXTRACT_BASE_DIR}" ] && export DOTNET_BUNDLE_EXTRACT_BASE_DIR="${XDG_CACHE_HOME:-"${HOME}"/.cache}/dotnet_bundle_extract"
