e1d05dc6ea6db1957b2a589d95200336b2965aca
Horațiu Mlendea <Horatiu.Mlendea@ProtonMail.com>
2021-11-17 10:57:01 +0200

    Fixes

--- scripts/uninstall-pkgs.sh
+++ scripts/uninstall-pkgs.sh
@@ -9,7 +9,7 @@ UNUSED_DEPS_COUNT=$(echo "${UNUSED_DEPS}" | wc -w)
 
 if [ "${UNUSED_DEPS_COUNT}" -gt 0 ]; then
     echo "Uninstalling unused dependencies ($UNUSED_DEPS_COUNT):"
-    uninstall-pkgs "${UNUSED_DEPS}"
+    uninstall-pkgs "${UNUSED_DEPS[@]}"
 fi
 
 if [[ "${DISTRO_FAMILY}" == "Arch" ]]; then
