b8dfecc1df2f5391531f9c2585d3bf4327851df2
Horațiu Mlendea <Horatiu.Mlendea@ProtonMail.com>
2021-11-18 16:49:09 +0200

    Improved package removal and cleaning

--- scripts/uninstall-pkgs.sh
+++ scripts/uninstall-pkgs.sh
@@ -2,18 +2,27 @@
 source "scripts/common/common.sh"
 source "scripts/common/package-management.sh"
 
-[[ "${DISTRO_FAMILY}" != "Arch" ]] && exit
+# Remove unused dependencies
+if [ "${DISTRO_FAMILY}" = "Arch" ]; then
+    UNUSED_DEPS=$(pacman -Qdtq)
+    UNUSED_DEPS_COUNT=$(echo "${UNUSED_DEPS}" | wc -w)
 
-UNUSED_DEPS=$(pacman -Qdtq)
-UNUSED_DEPS_COUNT=$(echo "${UNUSED_DEPS}" | wc -w)
-
-if [ "${UNUSED_DEPS_COUNT}" -gt 0 ]; then
-    echo "Uninstalling unused dependencies ($UNUSED_DEPS_COUNT):"
-    uninstall-pkgs "${UNUSED_DEPS[@]}"
+    if [ "${UNUSED_DEPS_COUNT}" -gt 0 ]; then
+        echo "Uninstalling unused dependencies ($UNUSED_DEPS_COUNT):"
+        uninstall-pkgs "${UNUSED_DEPS[@]}"
+    fi
 fi
 
-if [[ "${DISTRO_FAMILY}" == "Arch" ]]; then
+# Uninstall the packages
+if [ "${DISTRO_FAMILY}" = "Arch" ]; then
     uninstall-pkg "grub2-theme-vimix"   # Replaced by grub2-theme-nuci
     uninstall-pkg "ttf-ms-fonts"        # Replaced by ttf-ms-win10
     uninstall-pkg "yaourt-auto-sync"    # Replaced by repo-synchroniser
+fi
+
+# Clean the package cache
+if [ "${DISTRO_FAMILY}" = "Arch" ]; then
+    echo "Cleaning the package cache..."
+    run-as-su paccache -ruk0 # Remove all uninstall packages from the cache
+    run-as-su paccache -rk1  # Remove all cached versions of all packages, except the latest one
 fi
