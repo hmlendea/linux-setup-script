dec4fdc0cbd6b0e5896b2170ccfa7fcee58f4b77
Horațiu Mlendea <io.github.hmlendea.cnzq9@SimpleLogin.fr>
2021-12-25 12:01:31 +0200

    Updated uninstall-pkgs.sh

--- scripts/uninstall-pkgs.sh
+++ scripts/uninstall-pkgs.sh
@@ -15,10 +15,11 @@ fi
 
 # Uninstall the packages
 if [ "${DISTRO_FAMILY}" = "Arch" ]; then
-    uninstall-pkg "dialect"             # Depends on outdated libs
-    uninstall-pkg "grub2-theme-vimix"   # Replaced by grub2-theme-nuci
-    uninstall-pkg "ttf-ms-fonts"        # Replaced by ttf-ms-win10
-    uninstall-pkg "yaourt-auto-sync"    # Replaced by repo-synchroniser
+    uninstall-pkg "dialect"                 # Depends on outdated libs
+    uninstall-pkg "grub2-theme-vimix"       # Replaced by grub2-theme-nuci
+    uninstall-pkg "paper-icon-theme-git"    # Replaced by paper-icon-theme
+    uninstall-pkg "ttf-ms-fonts"            # Replaced by ttf-ms-win10
+    uninstall-pkg "yaourt-auto-sync"        # Replaced by repo-synchroniser
 fi
 
 # Clean the package cache
