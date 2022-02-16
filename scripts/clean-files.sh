dce139b4df24aa021111a7c69e88d2cca1661ad5
Horațiu Mlendea <io.github.hmlendea.cnzq9@SimpleLogin.fr>
2022-02-16 16:56:27 +0200

    Clean more files

--- scripts/clean-files.sh
+++ scripts/clean-files.sh
@@ -21,17 +21,25 @@ function removeDirectoryForMissingGnomeShellExtension() {
     done
 }
 
+removeDirectoryForMissingBin "aircrack-ng" "${HOME}/.aircrack"
 removeDirectoryForMissingBin "alsi" "${HOME_CONFIG}/alsi"
 removeDirectoryForMissingBin "audacity" "${HOME}/.audacity-data"
-removeDirectoryForMissingBin "autokey-shell" "${HOME_LOCAL_SHARE}/autokey"
+removeDirectoryForMissingBin "autokey-shell" \
+    "${HOME_CONFIG}/autokey" \
+    "${HOME_LOCAL_SHARE}/autokey"
 removeDirectoryForMissingBin "avidemux" "${HOME}/.avidemux6"
-removeDirectoryForMissingBin "bleachbit" "${HOME_CACHE}/bleachbit"
+removeDirectoryForMissingBin "bleachbit" \
+    "${HOME_CACHE}/bleachbit" \
+    "${HOME_CONFIG}/bleachbit"
 removeDirectoryForMissingBin "blesh" "${HOME}/.blerc"
 removeDirectoryForMissingBin "brave" \
     "${HOME_CACHE}/BraveSoftware" \
     "${HOME_CONFIG}/BraveSoftware"
 removeDirectoryForMissingBin "brz" "${HOME_CACHE}/breezy"
 removeDirectoryForMissingBin "cairo-dock" "${HOME_CONFIG}/cairo-dock"
+removeDirectoryForMissingBin "chiaki" \
+    "${HOME_CONFIG}/Chiaki" \
+    "${HOME_LOCAL_SHARE}/Chiaki"
 removeDirectoryForMissingBin "chromium" \
     "${HOME_CACHE}/chromium" \
     "${HOME_CONFIG}/chromium"
@@ -44,6 +52,7 @@ removeDirectoryForMissingBin "code-oss" \
     "${HOME}/.vscode-oss"
 removeDirectoryForMissingBin "codium" "${HOME_CONFIG}/VSCodium"
 (! does-bin-exist "code-oss") && (! does-bin-exist "codium") && remove "${HOME}/.vscode-oss"
+removeDirectoryForMissingBin "discord" "${HOME_CONFIG}/discord"
 removeDirectoryForMissingBin "dockx" "${HOME_LOCAL_SHARE}/dockbarx"
 #removeDirectoryForMissingBin "evolution" \
 #    "${HOME_CACHE}/evolution" \
@@ -58,6 +67,7 @@ removeDirectoryForMissingBin "gnubg" "${HOME}/.gnubg"
 removeDirectoryForMissingBin "google-chrome" \
     "${HOME_CACHE}/google-chrome" \
     "${HOME_CONFIG}/google-chrome"
+removeDirectoryForMissingBin "hashcat" "${HOME_CONFIG}/hashcat"
 removeDirectoryForMissingBin "kupfer" "${HOME_CONFIG}/kupfer"
 removeDirectoryForMissingBin "libreoffice" "${HOME_CONFIG}/libreoffice"
 removeDirectoryForMissingBin "lollypop" "${HOME_LOCAL_SHARE}/lollypop"
@@ -67,10 +77,12 @@ removeDirectoryForMissingBin "mcaselector" \
     "${HOME_CACHE}/mcaselector"
 removeDirectoryForMissingBin "mcedit" "${HOME}/.mcedit"
 removeDirectoryForMissingBin "minetest" "${HOME_CACHE}/minetest"
+removeDirectoryForMissingBin "mpv" "${HOME_CONFIG}/mpv"
 removeDirectoryForMissingBin "notion-app" "${HOME_CONFIG}/Notion"
 removeDirectoryForMissingBin "onlyoffice-desktopeditors" \
     "${HOME_CONFIG}/onlyoffice" \
     "${HOME_LOCAL_SHARE}/onlyoffice"
+removeDirectoryForMissingBin "openshot-qt" "${HOME}/.openshot_qt"
 removeDirectoryForMissingBin "pavucontrol" "${HOME_CONFIG}/pavucontrol.ini"
 removeDirectoryForMissingBin "pcmanfm" "${HOME_CONFIG}/pcmanfm"
 removeDirectoryForMissingBin "pcmanfm-qt" "${HOME_CONFIG}/pcmanfm-qt"
@@ -85,18 +97,27 @@ removeDirectoryForMissingBin "teams" \
 removeDirectoryForMissingBin "teams-insiders" \
     "${HOME_CONFIG}/Microsoft Teams - Insiders" \
     "${HOME_CONFIG}/Microsoft/Microsoft Teams - Insiders"
+removeDirectoryForMissingBin "teamviewer" "${HOME_LOCAL_SHARE}/teamviewer15"
 removeDirectoryForMissingBin "thunar" "${HOME_CONFIG}/Thunar"
 removeDirectoryForMissingBin "transmission-daemon" "${HOME_CONFIG}/transmission-daemon"
 removeDirectoryForMissingBin "ulauncher" "${HOME_LOCAL_SHARE}/ulauncher"
+removeDirectoryForMissingBin "vim" \
+    "${HOME}/.vimrc" \
+    "${HOME_CACHE}/vim"
 removeDirectoryForMissingBin "vlc" \
     "${HOME_CACHE}/vlc" \
     "${HOME_CONFIG}/vlc"
 removeDirectoryForMissingBin "whatsapp-nativefier" "${HOME_CONFIG}/whatsapp-nativefier-d40211"
 removeDirectoryForMissingBin "whatsdesk" "${HOME}/.whatsdesk"
 removeDirectoryForMissingBin "wike" "${HOME_CACHE}/wike"
+removeDirectoryForMissingBin "winetricks" "${HOME_CACHE}/winetricks"
 removeDirectoryForMissingBin "yarn" \
     "${HOME}/.yarn" \
     "${HOME}/.yarnrc"
+removeDirectoryForMissingBin "yay" "${HOME_CACHE}/yay"
 removeDirectoryForMissingBin "youtube-dl" "${HOME_CACHE}/youtube-dl"
 
 removeDirectoryForMissingGnomeShellExtension "tiling-assistant" "${HOME_CONFIG}/tiling-assistant"
+
+# Logs
+remove "${HOME}/.minecraft/logs"
