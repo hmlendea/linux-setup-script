eafcb1330ff393e9d579f9dc469fdb2e16c0a367
Horațiu Mlendea <io.github.hmlendea.cnzq9@SimpleLogin.fr>
2022-02-22 20:45:29 +0200

    Updated the Pi-Hole installation script

--- scripts/install-pihole.sh
+++ scripts/install-pihole.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
+source "scripts/common/common.sh"
+source "scripts/common/config.sh"
 source "scripts/common/package-management.sh"
+source "scripts/common/service-management.sh"
+
+DNS_CACHE_TTL=20 # Minutes
+DNS_CACHE_SIZE=10000 # Entries
+
+PIHOLE_DNSMASQ_CONFIG_PATH="${ROOT_ETC}/dnsmasq.d/01-pihole.conf"
 
 install-pkg pi-hole-server
 install-pkg php-sqlite
@@ -7,7 +15,7 @@ install-pkg php-sqlite
 install-dep lighttpd
 install-dep php-cgi
 
-sudo cp "/usr/share/pihole/configs/lighttpd.example.conf" "/etc/lighttpd/lighttpd.conf"
+update-file-if-needed "/usr/share/pihole/configs/lighttpd.example.conf" "/etc/lighttpd/lighttpd.conf"
 
 sudo sed -i 's/^;\(extension=pdo_sqlite\)$/\1/g' "/etc/php/php.ini"
 sudo sed -i 's/^;\(extension=sockets\)$/\1/g' "/etc/php/php.ini"
@@ -16,19 +24,18 @@ sudo sed -i 's/^server\.port.*$/server.port = 8093/' "/etc/lighttpd/lighttpd.con
 sudo sed -i 's/^#DBINTERVAL=.*/DBINTERVAL=60.0/' "/etc/pihole/pihole-FTL.conf"
 sudo sed -i 's/^#IGNORE_LOCALHOST=.*/IGNORE_LOCALHOST=yes/' "/etc/pihole/pihole-FTL.conf"
 
-set_config_value "/etc/dnsmasq.d/01-pihole.conf" "local-ttl" 3600 # 60 minutes
-set_config_value "/etc/dnsmasq.d/01-pihole.conf" "min-cache-ttl" 900 # 15 minutes
+set_config_value "${PIHOLE_DNSMASQ_CONFIG_PATH}" "cache-size" $((DNS_CACHE_SIZE))
+set_config_value "${PIHOLE_DNSMASQ_CONFIG_PATH}" "local-ttl" $((DNS_CACHE_TTL*60*3))
+set_config_value "${PIHOLE_DNSMASQ_CONFIG_PATH}" "min-cache-ttl" $((DNS_CACHE_TTL*60))
 
-sudo systemctl disable systemd-resolved
-sudo systemctl enable pihole-FTL
-sudo systemctl enable lighttpd
-
-sudo systemctl stop systemd-resolved
-sudo systemctl start pihole-FTL
-sudo systemctl start lighttpd
+disable_service "systemd-resolved"
+enable_service "pihole-FTL"
+enable_service "lighttpd"
 
 sudo usermod -a -G http pihole
 
 pihole -a -p
 
-echo "UPDATE THE HOSTS FILE!!!"
+for I in {1..5}; do
+    echo "UPDATE THE HOSTS FILE!!!    RESTART THE SYSTEM!!!"
+done
