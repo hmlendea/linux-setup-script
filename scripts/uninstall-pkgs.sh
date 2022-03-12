629fada1ed2e8217bf55e8d549aaea4a63e23d54
Horațiu Mlendea <Horatiu.Mlendea@ProtonMail.com>
2022-03-12 10:57:05 +0200

    Replace Simplenote with the flatpak version

--- scripts/uninstall-pkgs.sh
+++ scripts/uninstall-pkgs.sh
@@ -15,13 +15,15 @@ fi
 
 # Uninstall the packages
 if [ "${DISTRO_FAMILY}" = "Arch" ]; then
-    uninstall-pkg "alsi"                    # Replaced by fastfetch-git
-    uninstall-pkg "dialect"                 # Depends on outdated libs
-    uninstall-pkg "grub2-theme-vimix"       # Replaced by grub2-theme-nuci
-    uninstall-pkg "neofetch"                # Replaced by fastfetch-git
-    uninstall-pkg "paper-icon-theme-git"    # Replaced by paper-icon-theme
-    uninstall-pkg "ttf-ms-fonts"            # Replaced by ttf-ms-win10
-    uninstall-pkg "yaourt-auto-sync"        # Replaced by repo-synchroniser
+    uninstall-pkg "alsi"                        # Replaced by fastfetch-git
+    uninstall-pkg "dialect"                     # Depends on outdated libs
+    uninstall-pkg "grub2-theme-vimix"           # Replaced by grub2-theme-nuci
+    uninstall-pkg "neofetch"                    # Replaced by fastfetch-git
+    uninstall-pkg "paper-icon-theme-git"        # Replaced by paper-icon-theme
+    uninstall-pkg "simplenote-electron-bin"     # Replaced by flatpak: com.simplenote.Simplenote
+    uninstall-pkg "simplenote-electron-arm-bin" # Replaced by flatpak: com.simplenote.Simplenote
+    uninstall-pkg "ttf-ms-fonts"                # Replaced by ttf-ms-win10
+    uninstall-pkg "yaourt-auto-sync"            # Replaced by repo-synchroniser
 
     if is-package-installed "visual-studio-code-bin"; then
         uninstall-pkg "code"
@@ -32,7 +34,7 @@ fi
 # Clean the package cache
 if [ "${DISTRO_FAMILY}" = "Arch" ]; then
     echo "Cleaning the package cache..."
-    if does-bin-exist "paccace"; then
+    if does-bin-exist "paccache"; then
         run-as-su paccache -ruk0 # Remove all uninstall packages from the cache
         run-as-su paccache -rk1  # Remove all cached versions of all packages, except the latest one
     fi
