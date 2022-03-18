898ed9715ddb549b88974d1f5c52d7b40431e221
Horațiu Mlendea <Horatiu.Mlendea@ProtonMail.com>
2022-03-18 12:26:36 +0200

    Configure more permissions

--- scripts/configure-permissions.sh
+++ scripts/configure-permissions.sh
@@ -15,35 +15,49 @@ function get_flatpak_permission() {
 }
 
 function set_flatpak_permission() {
-    local PACKAGE="${1}"
-    local PERMISSION="${2}"
-    local VALUE="${3}"
+    local PACKAGE="${1}" && shift
 
     ! is_flatpak_installed "${PACKAGE}" && return
 
-    local TABLE=$(echo "${PERMISSION}" | awk -F":" '{print $1}')
-    local OBJECT=$(echo "${PERMISSION}" | awk -F":" '{print $2}')
-
-    [ -z "${OBJECT}" ] && OBJECT="${TABLE}"
-
-    PERMISSION="${TABLE}:${OBJECT}"
-
-    [[ "${VALUE}" == "true" ]] && VALUE="yes"
-    [[ "${VALUE}" == "false" ]] && VALUE="no"
-
-    local CURRENT_VALUE=$(get_flatpak_permission "${PACKAGE}" "${TABLE}" "${OBJECT}")
-
-    if [[ "${VALUE}" != "${CURRENT_VALUE}" ]]; then
-        flatpak permission-set "${TABLE}" "${OBJECT}" "${PACKAGE}" "${VALUE}"
-        echo -e "\e[0;33m${PACKAGE}\e[0m permission \e[0;32m${PERMISSION}\e[0m >>> ${VALUE}"
+    if [ "$(( $# % 2))" -ne 0 ]; then
+        echo "ERROR: Invalid arguments (count: $#) for set_launcher_entries: ${*}" >&2
+        exit 1
     fi
+
+    local PAIRS_COUNT=$(($# / 2))
+
+    for I in $(seq 1 ${PAIRS_COUNT}); do
+        local PERMISSION="${1}" && shift
+        local VALUE="${1}" && shift
+
+        local TABLE=$(echo "${PERMISSION}" | awk -F":" '{print $1}')
+        local OBJECT=$(echo "${PERMISSION}" | awk -F":" '{print $2}')
+
+        [ -z "${OBJECT}" ] && OBJECT="${TABLE}"
+
+        PERMISSION="${TABLE}:${OBJECT}"
+
+        [[ "${VALUE}" == "true" ]] && VALUE="yes"
+        [[ "${VALUE}" == "false" ]] && VALUE="no"
+
+        local CURRENT_VALUE=$(get_flatpak_permission "${PACKAGE}" "${TABLE}" "${OBJECT}")
+
+        if [[ "${VALUE}" != "${CURRENT_VALUE}" ]]; then
+           flatpak permission-set "${TABLE}" "${OBJECT}" "${PACKAGE}" "${VALUE}"
+            echo -e "\e[0;33m${PACKAGE}\e[0m permission \e[0;32m${PERMISSION}\e[0m >>> ${VALUE}"
+        fi
+    done
 }
 
 if does_bin_exist "flatpak"; then
     set_flatpak_permission "com.microsoft.Teams" "background" false
     set_flatpak_permission "com.mojang.Minecraft" "background" false
-    set_flatpak_permission "com.getpostman.Postman" "background" false
-    set_flatpak_permission "com.simplenote.Simplenote" "background" false
+    set_flatpak_permission "com.getpostman.Postman" \
+        "background" false \
+        "notifications:notification" false
+    set_flatpak_permission "com.simplenote.Simplenote" \
+        "background" false \
+        "notifications:notification" false
     set_flatpak_permission "io.github.hmlendea.geforcenow-electron" "background" false
     set_flatpak_permission "org.gnome.baobab" "background" false
     set_flatpak_permission "org.gnome.Calculator" "background" false
@@ -51,8 +65,14 @@ if does_bin_exist "flatpak"; then
     set_flatpak_permission "org.gnome.Contacts" "background" false
     set_flatpak_permission "org.gnome.eog" "background" false
     set_flatpak_permission "org.gnome.Evince" "background" false
+    set_flatpak_permission "org.gnome.gedit" \
+        "background" false \
+        "notifications:notification" false
     set_flatpak_permission "org.gnome.Maps" "background" false
     set_flatpak_permission "org.gnome.Rhythmbox3" "background" false
+    set_flatpak_permission "org.gnome.TextEditor" \
+        "background" false \
+        "notifications:notification" false
     set_flatpak_permission "org.gnome.Totem" "background" false
     set_flatpak_permission "org.gnome.Weather" "background" false
     set_flatpak_permission "org.inkscape.Inkscape" "background" false
