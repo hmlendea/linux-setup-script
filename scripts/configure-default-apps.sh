872f3898270bbc34f1545dbee832a286bdae2d5a
Horațiu Mlendea <io.github.hmlendea.cnzq9@SimpleLogin.fr>
2022-05-28 13:31:50 +0300

    Configure the default apps for some mimetypes

---
+++ scripts/configure-default-apps.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+source "scripts/common/filesystem.sh"
+source "${REPO_DIR}/scripts/common/common.sh"
+source "${REPO_DIR}/scripts/common/package-management.sh"
+
+function update_mimetype_association() {
+    local MIMETYPE="${1}"
+    local LAUNCHER="${2}"
+
+    local MIMEAPPS_FILE="${HOME_CONFIG}/mimeapps.list"
+
+    grep -q "^${MIMETYPE}=${LAUNCHER}$" "${MIMEAPPS_FILE}" && return
+
+    echo -e "Associating the \e[0;33m${MIMETYPE}\e[0m mimetype with \e[0;33m${LAUNCHER}\e[0m..."
+    local MIMETYPE_ESC=$(echo "${MIMETYPE}" | sed \
+                            -e 's/\//\\\//g')
+    sed -i '/^'"${MIMETYPE_ESC}"'=/d' "${MIMEAPPS_FILE}"
+    echo "${MIMETYPE}=${LAUNCHER}" >> "${MIMEAPPS_FILE}"
+}
+
+for IMAGE_TYPE in "bmp" "jpeg" "png" "webp"; do
+    update_mimetype_association "image/${IMAGE_TYPE}" "org.gnome.eog.desktop"
+done
+
+is_flatpak_installed "com.microsoft.Teams" && update_mimetype_association "x-scheme-handler/msteams" "com.microsoft.Teams.desktop"
+
+BROWSER_LAUNCHER=""
+TEXT_EDITOR_LAUNCHER=""
+
+is_flatpak_installed "org.mozilla.firefox" && BROWSER_LAUNCHER="org.mozilla.firefox.desktop"
+is_flatpak_installed "org.gnome.gedit" && TEXT_EDITOR_LAUNCHER="org.gnome.gedit.desktop"
+
+if [ -n "${BROWSER_LAUNCHER}" ]; then
+    for SCHEME_TYPE in "http" "https" "chrome"; do
+        update_mimetype_association "x-scheme-handler/${SCHEME_TYPE}" "${BROWSER_LAUNCHER}"
+    done
+    for EXTENSION_TYPE in "htm" "html" "shtml" "xht" "xhtml"; do
+        update_mimetype_association "application/x-extension-${EXTENSION_TYPE}" "${BROWSER_LAUNCHER}"
+    done
+
+    update_mimetype_association "application/xhtml+xml" "${BROWSER_LAUNCHER}"
+    update_mimetype_association "text/html" "${BROWSER_LAUNCHER}"
+fi
+
+if [ -n "${TEXT_EDITOR_LAUNCHER}" ]; then
+    for APPLICATION_TYPE in "json" "x-wine-extension-ini"; do
+        update_mimetype_association "application/${APPLICATION_TYPE}" "${TEXT_EDITOR_LAUNCHER}"
+    done
+
+    update_mimetype_association "audio/x-mod" "${TEXT_EDITOR_LAUNCHER}"
+    update_mimetype_association "text/plain" "${TEXT_EDITOR_LAUNCHER}"
+fi
