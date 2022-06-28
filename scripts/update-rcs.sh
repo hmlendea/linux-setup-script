0594acb407ea09a705235d3bd5cf484d190707be
Horațiu Mlendea <Horatiu.Mlendea@ProtonMail.com>
2022-06-28 11:17:04 +0300

    Moved more files and dirs away from the home dir

--- scripts/update-rcs.sh
+++ scripts/update-rcs.sh
@@ -21,10 +21,11 @@ for RC in "gimprc" "sessionrc" "toolrc"; do
     does_bin_exist "org.gimp.GIMP" && update_file_if_distinct "${REPO_RC_DIR}/gimp/${RC}" "${HOME_VAR_APP}/org.gimp.GIMP/config/GIMP/2.10/${RC}"
 done
 
-does_bin_exist "nano"       && update_file_if_distinct "${REPO_RC_DIR}/nanorc"        "${HOME}/.nanorc"
-does_bin_exist "vim"        && update_file_if_distinct "${REPO_RC_DIR}/vimrc"         "${HOME}/.vimrc"
-does_bin_exist "git"        && update_file_if_distinct "${REPO_RC_DIR}/gitconfig"     "${XDG_CONFIG_HOME}/git/config"
-does_bin_exist "lxpanel"    && update_file_if_distinct "${REPO_RC_DIR}/lxde-panel"    "${XDG_CONFIG_HOME}/lxpanel/LXDE/panels/panel"
+does_bin_exist "nano"       && update_file_if_distinct "${REPO_RC_DIR}/nanorc"      "${HOME}/.nanorc"
+does_bin_exist "npm"        && update_file_if_distinct "${REPO_RC_DIR}/npmrc"       "${HOME}/.npmrc"
+does_bin_exist "vim"        && update_file_if_distinct "${REPO_RC_DIR}/vimrc"       "${HOME}/.vimrc"
+does_bin_exist "git"        && update_file_if_distinct "${REPO_RC_DIR}/gitconfig"   "${XDG_CONFIG_HOME}/git/config"
+does_bin_exist "lxpanel"    && update_file_if_distinct "${REPO_RC_DIR}/lxde-panel"  "${XDG_CONFIG_HOME}/lxpanel/LXDE/panels/panel"
 #[ -f "${ROOT_USR_BIN}/lxpanel" ]   && copy_rc "lxde-dock" "${XDG_CONFIG_HOME}/lxpanel/LXDE/panels/dock"
 
 if does_bin_exist "firefox" "org.mozilla.firefox"; then
