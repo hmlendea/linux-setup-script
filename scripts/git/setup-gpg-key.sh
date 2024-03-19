d3b2830ea9c037fec4a6b7eb281ce8d73f1cb34d
Horațiu Mlendea <io.github.hmlendea.cnzq9@SimpleLogin.fr>
2024-03-19 12:29:07 +0200

    Updated the git GPG script

--- scripts/git/setup-gpg-key.sh
+++ scripts/git/setup-gpg-key.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 source "scripts/common/filesystem.sh"
 
-GNPUPG_PATH="${XDG_DATA_HOME}/gnupg"
+GNUPG_PATH="${XDG_DATA_HOME}/gnupg"
 PUBRING_FILE_PATH="${GNUPG_PATH}/pubring.kbx"
 GITHUB_KEY_ID_FILE_PATH="${GNUPG_PATH}/github_key_id.txt"
 GITCONFIG_FILE_PATH="${XDG_CONFIG_HOME}/git/config"
@@ -10,12 +10,12 @@ if [ -f "${PUBRING_FILE_PATH}" ] &&
    [ -f "${GITHUB_KEY_ID_FILE_PATH}" ]; then
     GITHUB_GPG_KEY_ID=$(<"${GITHUB_KEY_ID_FILE_PATH}")
 
-    if [ -n "${GITHUB_GPG_KEY_ID}" ]; then
-        echo "Found GitHub GPG Key: ${GITHUB_GPG_KEY_ID}"
+    [ -z "${GITHUB_GPG_KEY_ID}" ] && exit
 
-        git config --global user.signingkey "${GITHUB_GPG_KEY_ID}"
-        git config --global commit.gpgsign true
+    echo "Found GitHub GPG Key: ${GITHUB_GPG_KEY_ID}"
 
-        sed -i 's/\t/    /g' "${GITCONFIG_FILE_PATH}"
-    fi
+    git config --global user.signingkey "${GITHUB_GPG_KEY_ID}"
+    git config --global commit.gpgsign true
+
+    sed -i 's/\t/    /g' "${GITCONFIG_FILE_PATH}"
 fi
