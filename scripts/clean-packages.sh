8144d33fe82f599ac86d45d37a453dbf06828024
Horațiu Mlendea <io.github.hmlendea.cnzq9@SimpleLogin.fr>
2025-01-16 10:11:15 +0200

    uClean packages on Alpine distros

--- scripts/clean-packages.sh
+++ scripts/clean-packages.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 source "scripts/common/filesystem.sh"
-source "${REPO_SCRIPTS_DIR}/common/common.sh"
-source "${REPO_SCRIPTS_DIR}/common/package-management.sh"
-source "${REPO_SCRIPTS_DIR}/common/system-info.sh"
+source "${REPO_SCRIPTS_COMMON_DIR}/common.sh"
+source "${REPO_SCRIPTS_COMMON_DIR}/package-management.sh"
+source "${REPO_SCRIPTS_COMMON_DIR}/system-info.sh"
 
-if [ "${DISTRO_FAMILY}" = 'Arch' ]; then
+if [ "${DISTRO_FAMILY}" = 'Alpine' ]; then
+    call_package_manager cache clean
+elif [ "${DISTRO_FAMILY}" = 'Arch' ]; then
     if does_bin_exist 'paccache'; then
         run_as_su paccache -ruk0 # Remove all uninstall packages from the cache
         run_as_su paccache -rk1  # Remove all cached versions of all packages, except the latest one
