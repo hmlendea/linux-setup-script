36b4f30c452e5b7b5c1a3bcf0cb63c5dd18b24f7
hmlendea <io.github.hmlendea.cnzq9@SimpleLogin.fr>
2025-12-06 10:05:41 +0200

    Improved system time config

--- scripts/configure-system-time.sh
+++ scripts/configure-system-time.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 source 'scripts/common/filesystem.sh'
+source "${REPO_SCRIPTS_COMMON_DIR}/common.sh"
+
+PREFERRED_TIMEZONE='Europe/Bucharest'
 
 LOCALE_CONF_FILE_PATH="${ROOT_ETC}/locale.conf"
 LOCALTIME_FILE_PATH="${ROOT_ETC}/localtime"
@@ -9,9 +12,12 @@ echo 'Setting up the time locale...'
     echo 'LC_TIME=ro_RO.UTF-8'
 } > "${LOCALE_CONF_FILE_PATH}"
 
-if [ ! -f "${LOCALTIME_FILE_PATH}" ]; then
+if does_bin_exist 'timedatectl'; then
+    run_as_su timedatectl set-timezone "${PREFERRED_TIMEZONE}"
+elif [ ! -f "${LOCALTIME_FILE_PATH}" ]; then
     echo 'Setting up the local time...'
 
-    ln -sf '/usr/share/zoneinfo/Europe/Bucharest' "${LOCALTIME_FILE_PATH}"
-    does_bin_exist 'hwclock' && hwclock --systohc
+    ln -sf "${ROOT_USER_SHARE}/zoneinfo/${PREFERRED_TIMEZONE}" "${LOCALTIME_FILE_PATH}"
 fi
+
+does_bin_exist 'hwclock' && hwclock --systohc
