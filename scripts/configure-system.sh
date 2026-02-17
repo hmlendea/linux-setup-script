909178d28358e311054f017984a4e70cbd59fe80
hmlendea <io.github.hmlendea.cnzq9@SimpleLogin.fr>
2026-02-17 11:05:15 +0200

    VSCode Pylance standard type checking

--- scripts/configure-system.sh
+++ scripts/configure-system.sh
@@ -1177,7 +1177,7 @@ if does_bin_exist 'code' 'code-oss' 'codium' 'com.visualstudio.code'; then
     set_json_property "${VSCODE_CONFIG_FILE}" '.["omnisharp.enableDecompilationSupport"]' true
     set_json_property "${VSCODE_CONFIG_FILE}" '.["dotnet.automaticallyCreateSolutionInWorkspace"]' false
     # Python
-    set_json_property "${VSCODE_CONFIG_FILE}" '.["python.analysis.typeCheckingMode"]' 'strict'
+    set_json_property "${VSCODE_CONFIG_FILE}" '.["python.analysis.typeCheckingMode"]' 'standard'
 
     # Terminal
     set_json_property "${VSCODE_CONFIG_FILE}" '.["terminal.integrated.shell.linux"]' "${SHELL}"
