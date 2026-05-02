0fdecf078a6356b9416fe3babaa7b2a6a8151916
Horațiu Mlendea <io.github.hmlendea.cnzq9@SimpleLogin.fr>
2026-05-02 23:17:55 +0300

    Automatic shutdown for Argon ONE UP

--- scripts/configure-hardware-integration.sh
+++ scripts/configure-hardware-integration.sh
@@ -8,15 +8,26 @@ source "${REPO_DIR}/scripts/common/system-info.sh"
 DEVICE_MODEL="$(get_device_model)"
 
 if [[ "${DEVICE_MODEL}" =~ 'Argon ONE UP' ]]; then
+    WORKING_DIRECTORY="$(pwd)"
+
     if ! ls "${ROOT_USR_SRC}"/oneUpPower-* >/dev/null 2>&1; then
-        WORKING_DIRECTORY="$(pwd)"
-        [ ! -d "${LOCAL_INSTALL_TEMP_DIR}" ] && mkdir -p "${LOCAL_INSTALL_TEMP_DIR}"
+        create_directory "${LOCAL_INSTALL_TEMP_DIR}"
         cd "${LOCAL_INSTALL_TEMP_DIR}"
 
         [ ! -d 'argon-oneup' ] && git clone 'https://github.com/JeffCurless/argon-oneup.git'
         cd 'argon-oneup'
 
         run_as_su 'battery/setup'
+    fi
+
+    if ! ls "${ROOT_USR_LOCAL_BIN}/argononeup-automatic-shutdown" >/dev/null 2>&1; then
+        create_directory "${LOCAL_INSTALL_TEMP_DIR}"
+        cd "${LOCAL_INSTALL_TEMP_DIR}"
+
+        [ ! -d 'argononeup-automatic-shutdown' ] && git clone 'https://github.com/hmlendea/argononeup-automatic-shutdown'
+        cd 'argononeup-automatic-shutdown'
+
+        run_as_su './install'
     fi
 
     disable_service 'argononeupd'
