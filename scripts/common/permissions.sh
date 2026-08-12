382fad916bd4f4fbc42d4f63f7eacb40fa2eb7b1
Horațiu Mlendea <io.github.hmlendea.cnzq9@SimpleLogin.fr>
2026-08-12 17:36:02 +0300

    Optimised the permissions script

--- scripts/common/permissions.sh
+++ scripts/common/permissions.sh
@@ -4,7 +4,7 @@ function set_linux_permission() {
     local APPLICATION="${1}" && shift
 
     if [ "$(( $# % 2))" -ne 0 ]; then
-        echo "ERROR: Invalid arguments (count: $#) for set_launcher_entries: ${*}" >&2
+        echo "ERROR: Invalid arguments (count: $#) for set_linux_permission: ${*}" >&2
         exit 1
     fi
 
@@ -18,17 +18,18 @@ function set_linux_permission() {
 
     local MICROPHONE_STATE=''
     local SPEAKERS_STATE=''
+    local NORMALISED_APPLICATION
+    local GSETTING_SCHEMA
 
-    local PAIRS_COUNT=$(($# / 2))
-    for I in $(seq 1 ${PAIRS_COUNT}); do
+    NORMALISED_APPLICATION=$(echo "${APPLICATION}" | sed 's/\./-/g' | tr '[:upper:]' '[:lower:]')
+    GSETTING_SCHEMA="org.gnome.desktop.notifications.application:/org/gnome/desktop/notifications/application/${NORMALISED_APPLICATION}/"
+
+    while [ $# -gt 0 ]; do
         local PERMISSION="${1}" && shift
         local STATE="${1}" && shift
 
         [ "${PERMISSION}" = 'microphone' ] && MICROPHONE_STATE="${STATE}"
         [ "${PERMISSION}" = 'speakers' ] && SPEAKERS_STATE="${STATE}"
-
-        local GSETTING_SCHEMA="/org/gnome/desktop/notifications/application/$(echo ${APPLICATION} | sed 's/\./-/g' | tr '[:upper:]' '[:lower:]')/"
-        GSETTING_SCHEMA="org.gnome.desktop.notifications.application:${GSETTING_SCHEMA}"
 
         if [ "${PERMISSION}" = 'notification' ]; then
             set_gsetting "${GSETTING_SCHEMA}" enable "${STATE}"
@@ -41,23 +42,32 @@ function set_linux_permission() {
         fi
 
         if ${IS_FLATPAK_INSTALLED}; then
-            if [ "${PERMISSION}" = 'background' ]; then
-                set_flatpak_permission "${APPLICATION}" 'background' 'background' "${STATE}"
-            elif [ "${PERMISSION}" = 'camera' ]; then
-                set_flatpak_permission "${APPLICATION}" 'devices' 'camera' "${STATE}"
-            elif [ "${PERMISSION}" = 'filesystem-home' ]; then
-                set_flatpak_filesystem "${APPLICATION}" 'home' "${STATE}"
-            elif [ "${PERMISSION}" = 'microphone' ]; then
-                set_flatpak_permission "${APPLICATION}" 'devices' 'microphone' "${STATE}"
-            elif [ "${PERMISSION}" = 'speakers' ]; then
-                set_flatpak_permission "${APPLICATION}" 'devices' 'speakers' "${STATE}"
-            elif [ "${PERMISSION}" = 'location' ]; then
-                set_flatpak_permission "${APPLICATION}" 'location' 'location' "${STATE}"
-            elif [ "${PERMISSION}" = 'network' ]; then
-                set_flatpak_shared "${APPLICATION}" 'network' "${STATE}"
-            elif [ "${PERMISSION}" = 'notification' ]; then
-                set_flatpak_permission "${APPLICATION}" 'notifications' 'notification' "${STATE}"
-            fi
+            case "${PERMISSION}" in
+                background)
+                    set_flatpak_permission "${APPLICATION}" 'background' 'background' "${STATE}"
+                    ;;
+                camera)
+                    set_flatpak_permission "${APPLICATION}" 'devices' 'camera' "${STATE}"
+                    ;;
+                filesystem-home)
+                    set_flatpak_filesystem "${APPLICATION}" 'home' "${STATE}"
+                    ;;
+                microphone)
+                    set_flatpak_permission "${APPLICATION}" 'devices' 'microphone' "${STATE}"
+                    ;;
+                speakers)
+                    set_flatpak_permission "${APPLICATION}" 'devices' 'speakers' "${STATE}"
+                    ;;
+                location)
+                    set_flatpak_permission "${APPLICATION}" 'location' 'location' "${STATE}"
+                    ;;
+                network)
+                    set_flatpak_shared "${APPLICATION}" 'network' "${STATE}"
+                    ;;
+                notification)
+                    set_flatpak_permission "${APPLICATION}" 'notifications' 'notification' "${STATE}"
+                    ;;
+            esac
         fi
     done
 
@@ -85,6 +95,15 @@ function get_flatpak_permission() {
     flatpak permission-show "${PACKAGE}" | grep "^${TABLE}\s${OBJECT}\s" | awk '{print $4}'
 }
 
+function get_flatpak_metadata_value() {
+    local METADATA_FILE="${1}"
+    local FIELD="${2}"
+
+    [ ! -f "${METADATA_FILE}" ] && return 1
+
+    grep "^${FIELD}=" "${METADATA_FILE}" | awk -F'=' '{print $2}'
+}
+
 function set_flatpak_permission() {
     local PACKAGE="${1}"
     local TABLE="${2}"
@@ -109,14 +128,8 @@ function set_flatpak_permission() {
 
     if [ "${VALUE}" != "${CURRENT_VALUE}" ]; then
         flatpak permission-set "${TABLE}" "${OBJECT}" "${PACKAGE}" "${VALUE}"
-        echo -e "\e[0;33m${PACKAGE}\e[0m permission \e[0;32m${PERMISSION}\e[0m >>> ${VALUE}"
+        echo -e "\e[0;33m${PACKAGE}\e[0m permission \e[0;32m${OBJECT}\e[0m >>> ${VALUE}"
     fi
-}
-
-function set_flatpak_device_access() {
-    local PACKAGE="${1}"
-    local DEVICE="${2}"
-    local HAS_ACCESS="${3}"
 }
 
 function get_flatpak_shared() {
@@ -125,9 +138,9 @@ function get_flatpak_shared() {
 
     for METADATA_FILE in "${ROOT_VAR_LIB}/flatpak/app/${APPLICATION}/current/active/metadata" \
                          "${XDG_DATA_HOME}/flatpak/app/${APPLICATION}/current/active/metadata"; do
-        [ ! -f "${METADATA_FILE}" ] && continue
+        local SHARED_OBJECTS="$(get_flatpak_metadata_value "${METADATA_FILE}" 'shared')"
 
-        local SHARED_OBJECTS=$(grep '^shared=' "${METADATA_FILE}" | awk -F'=' '{print $2}')
+        [ -z "${SHARED_OBJECTS}" ] && continue
 
         if echo "${SHARED_OBJECTS}" | grep -q "${OBJECT};"; then
             return 0 # True
@@ -141,17 +154,17 @@ function set_flatpak_shared() {
     local APPLICATION="${1}"
     local OBJECT="${2}"
     local STATE="${3}"
+    local CURRENT_STATE=false
+
+    get_flatpak_shared "${APPLICATION}" "${OBJECT}" && CURRENT_STATE=true
+
+    [ "${STATE}" = "${CURRENT_STATE}" ] && return
 
     for METADATA_FILE in "${ROOT_VAR_LIB}/flatpak/app/${APPLICATION}/current/active/metadata" \
                          "${XDG_DATA_HOME}/flatpak/app/${APPLICATION}/current/active/metadata"; do
-        [ ! -f "${METADATA_FILE}" ] && continue
+        local SHARED_OBJECTS="$(get_flatpak_metadata_value "${METADATA_FILE}" 'shared')"
 
-        local SHARED_OBJECTS=$(grep '^shared=' "${METADATA_FILE}" | awk -F'=' '{print $2}')
-        local CURRENT_STATE=false
-
-        get_flatpak_shared "${APPLICATION}" "${OBJECT}" && CURRENT_STATE=true
-
-        [ "${STATE}" = "${CURRENT_STATE}" ] && return
+        [ -z "${SHARED_OBJECTS}" ] && continue
 
         if ${STATE}; then
             SHARED_OBJECTS="${OBJECT};${SHARED_OBJECTS}"
@@ -202,61 +215,76 @@ function set_android_permission() {
 
     ! is_android_package_installed "${PACKAGE}" && return
 
-    local PAIRS_COUNT=$(($# / 2))
-    for I in $(seq 1 ${PAIRS_COUNT}); do
+    while [ $# -gt 0 ]; do
         local PERMISSION="${1}" && shift
         local VALUE="${1}" && shift
 
-        if [ "${PERMISSION}" = 'accounts_get' ]; then
-            toggle_android_permission "${PACKAGE}" 'android.permission.GET_ACCOUNTS' "${VALUE}"
-        elif [ "${PERMISSION}" = 'calendar' ]; then
-            toggle_android_permission "${PACKAGE}" 'android.permission.READ_CALENDAR' "${VALUE}"
-            toggle_android_permission "${PACKAGE}" 'android.permission.WRITE_CALENDAR' "${VALUE}"
-        elif [ "${PERMISSION}" = 'camera' ]; then
-            toggle_android_permission "${PACKAGE}" 'android.permission.CAMERA' "${VALUE}"
-        elif [ "${PERMISSION}" = 'contacts' ]; then
-            toggle_android_permission "${PACKAGE}" 'android.permission.READ_CONTACTS' "${VALUE}"
-            toggle_android_permission "${PACKAGE}" 'android.permission.WRITE_CONTACTS' "${VALUE}"
-        elif [ "${PERMISSION}" = 'contacts_read' ]; then
-            toggle_android_permission "${PACKAGE}" 'android.permission.READ_CONTACTS' "${VALUE}"
-        elif [ "${PERMISSION}" = 'contacts_write' ]; then
-            toggle_android_permission "${PACKAGE}" 'android.permission.WRITE_CONTACTS' "${VALUE}"
-        elif [ "${PERMISSION}" = 'location' ]; then
-            toggle_android_permission "${PACKAGE}" 'android.permission.ACCESS_COARSE_LOCATION' "${VALUE}"
-            toggle_android_permission "${PACKAGE}" 'android.permission.ACCESS_FINE_LOCATION' "${VALUE}"
-        elif [ "${PERMISSION}" = 'location' ]; then
-            toggle_android_permission "${PACKAGE}" 'android.permission.ACCESS_COARSE_LOCATION' "${VALUE}"
-            toggle_android_permission "${PACKAGE}" 'android.permission.ACCESS_FINE_LOCATION' "${VALUE}"
-        elif [ "${PERMISSION}" = 'location_background' ]; then
-            toggle_android_permission "${PACKAGE}" 'android.permission.ACCESS_BACKGROUND_LOCATION' "${VALUE}"
-        elif [ "${PERMISSION}" = 'microphone' ]; then
-            toggle_android_permission "${PACKAGE}" 'android.permission.RECORD_AUDIO' "${VALUE}"
-        elif [ "${PERMISSION}" = 'phone_log' ]; then
-            toggle_android_permission "${PACKAGE}" 'android.permission.READ_CALL_LOG' "${VALUE}"
-        elif [ "${PERMISSION}" = 'phone' ]; then
-            toggle_android_permission "${PACKAGE}" 'android.permission.ANSWER_PHONE_CALLS' "${VALUE}"
-            toggle_android_permission "${PACKAGE}" 'android.permission.CALL_PHONE' "${VALUE}"
-            toggle_android_permission "${PACKAGE}" 'android.permission.READ_PHONE_NUMBERS' "${VALUE}"
-            toggle_android_permission "${PACKAGE}" 'android.permission.READ_PHONE_STATE' "${VALUE}"
-        elif [ "${PERMISSION}" = 'physical_activity' ]; then
-            toggle_android_permission "${PACKAGE}" "android.permission.ACTIVITY_RECOGNITION" "${VALUE}"
-        elif [ "${PERMISSION}" = 'sms' ]; then
-            toggle_android_permission "${PACKAGE}" 'android.permission.READ_SMS' "${VALUE}"
-            toggle_android_permission "${PACKAGE}" 'android.permission.RECEIVE_MMS' "${VALUE}"
-            toggle_android_permission "${PACKAGE}" 'android.permission.RECEIVE_SMS' "${VALUE}"
-            toggle_android_permission "${PACKAGE}" 'android.permission.SEND_SMS' "${VALUE}"
-        elif [ "${PERMISSION}" = 'storage' ]; then
-            toggle_android_permission "${PACKAGE}" 'android.permission.READ_EXTERNAL_STORAGE' "${VALUE}"
-            toggle_android_permission "${PACKAGE}" 'android.permission.WRITE_EXTERNAL_STORAGE' "${VALUE}"
-        elif [ "${PERMISSION}" = 'storage_read' ]; then
-            toggle_android_permission "${PACKAGE}" 'android.permission.READ_EXTERNAL_STORAGE' "${VALUE}"
-        elif [ "${PERMISSION}" = 'storage_media' ]; then
-            toggle_android_permission "${PACKAGE}" 'android.permission.ACCESS_MEDIA_LOCATION' "${VALUE}"
-        elif [ "${PERMISSION}" = 'storage_write' ]; then
-            toggle_android_permission "${PACKAGE}" 'android.permission.WRITE_EXTERNAL_STORAGE' "${VALUE}"
-        else
-            toggle_android_permission "${PACKAGE}" "${PERMISSION}" "${VALUE}"
-        fi
+        case "${PERMISSION}" in
+            accounts_get)
+                toggle_android_permission "${PACKAGE}" 'android.permission.GET_ACCOUNTS' "${VALUE}"
+                ;;
+            calendar)
+                toggle_android_permission "${PACKAGE}" 'android.permission.READ_CALENDAR' "${VALUE}"
+                toggle_android_permission "${PACKAGE}" 'android.permission.WRITE_CALENDAR' "${VALUE}"
+                ;;
+            camera)
+                toggle_android_permission "${PACKAGE}" 'android.permission.CAMERA' "${VALUE}"
+                ;;
+            contacts)
+                toggle_android_permission "${PACKAGE}" 'android.permission.READ_CONTACTS' "${VALUE}"
+                toggle_android_permission "${PACKAGE}" 'android.permission.WRITE_CONTACTS' "${VALUE}"
+                ;;
+            contacts_read)
+                toggle_android_permission "${PACKAGE}" 'android.permission.READ_CONTACTS' "${VALUE}"
+                ;;
+            contacts_write)
+                toggle_android_permission "${PACKAGE}" 'android.permission.WRITE_CONTACTS' "${VALUE}"
+                ;;
+            location)
+                toggle_android_permission "${PACKAGE}" 'android.permission.ACCESS_COARSE_LOCATION' "${VALUE}"
+                toggle_android_permission "${PACKAGE}" 'android.permission.ACCESS_FINE_LOCATION' "${VALUE}"
+                ;;
+            location_background)
+                toggle_android_permission "${PACKAGE}" 'android.permission.ACCESS_BACKGROUND_LOCATION' "${VALUE}"
+                ;;
+            microphone)
+                toggle_android_permission "${PACKAGE}" 'android.permission.RECORD_AUDIO' "${VALUE}"
+                ;;
+            phone_log)
+                toggle_android_permission "${PACKAGE}" 'android.permission.READ_CALL_LOG' "${VALUE}"
+                ;;
+            phone)
+                toggle_android_permission "${PACKAGE}" 'android.permission.ANSWER_PHONE_CALLS' "${VALUE}"
+                toggle_android_permission "${PACKAGE}" 'android.permission.CALL_PHONE' "${VALUE}"
+                toggle_android_permission "${PACKAGE}" 'android.permission.READ_PHONE_NUMBERS' "${VALUE}"
+                toggle_android_permission "${PACKAGE}" 'android.permission.READ_PHONE_STATE' "${VALUE}"
+                ;;
+            physical_activity)
+                toggle_android_permission "${PACKAGE}" 'android.permission.ACTIVITY_RECOGNITION' "${VALUE}"
+                ;;
+            sms)
+                toggle_android_permission "${PACKAGE}" 'android.permission.READ_SMS' "${VALUE}"
+                toggle_android_permission "${PACKAGE}" 'android.permission.RECEIVE_MMS' "${VALUE}"
+                toggle_android_permission "${PACKAGE}" 'android.permission.RECEIVE_SMS' "${VALUE}"
+                toggle_android_permission "${PACKAGE}" 'android.permission.SEND_SMS' "${VALUE}"
+                ;;
+            storage)
+                toggle_android_permission "${PACKAGE}" 'android.permission.READ_EXTERNAL_STORAGE' "${VALUE}"
+                toggle_android_permission "${PACKAGE}" 'android.permission.WRITE_EXTERNAL_STORAGE' "${VALUE}"
+                ;;
+            storage_read)
+                toggle_android_permission "${PACKAGE}" 'android.permission.READ_EXTERNAL_STORAGE' "${VALUE}"
+                ;;
+            storage_media)
+                toggle_android_permission "${PACKAGE}" 'android.permission.ACCESS_MEDIA_LOCATION' "${VALUE}"
+                ;;
+            storage_write)
+                toggle_android_permission "${PACKAGE}" 'android.permission.WRITE_EXTERNAL_STORAGE' "${VALUE}"
+                ;;
+            *)
+                toggle_android_permission "${PACKAGE}" "${PERMISSION}" "${VALUE}"
+                ;;
+        esac
     done
 }
 
@@ -266,9 +294,9 @@ function get_flatpak_filesystem() {
 
     for METADATA_FILE in "${ROOT_VAR_LIB}/flatpak/app/${APPLICATION}/current/active/metadata" \
                          "${XDG_DATA_HOME}/flatpak/app/${APPLICATION}/current/active/metadata"; do
-        [ ! -f "${METADATA_FILE}" ] && continue
+        local FILESYSTEMS="$(get_flatpak_metadata_value "${METADATA_FILE}" 'filesystems')"
 
-        local FILESYSTEMS=$(grep '^filesystems=' "${METADATA_FILE}" | awk -F'=' '{print $2}')
+        [ -z "${FILESYSTEMS}" ] && continue
 
         if echo "${FILESYSTEMS}" | grep -q "^${FILESYSTEM};\|;${FILESYSTEM};\|;${FILESYSTEM}$\|^${FILESYSTEM}$"; then
             return 0
@@ -282,17 +310,17 @@ function set_flatpak_filesystem() {
     local APPLICATION="${1}"
     local FILESYSTEM="${2}"
     local STATE="${3}"
+    local CURRENT_STATE=false
+
+    get_flatpak_filesystem "${APPLICATION}" "${FILESYSTEM}" && CURRENT_STATE=true
+
+    [ "${STATE}" = "${CURRENT_STATE}" ] && return
 
     for METADATA_FILE in "${ROOT_VAR_LIB}/flatpak/app/${APPLICATION}/current/active/metadata" \
                          "${XDG_DATA_HOME}/flatpak/app/${APPLICATION}/current/active/metadata"; do
-        [ ! -f "${METADATA_FILE}" ] && continue
+        local FILESYSTEMS="$(get_flatpak_metadata_value "${METADATA_FILE}" 'filesystems')"
 
-        local FILESYSTEMS=$(grep '^filesystems=' "${METADATA_FILE}" | awk -F'=' '{print $2}')
-        local CURRENT_STATE=false
-
-        get_flatpak_filesystem "${APPLICATION}" "${FILESYSTEM}" && CURRENT_STATE=true
-
-        [ "${STATE}" = "${CURRENT_STATE}" ] && return
+        [ -z "${FILESYSTEMS}" ] && continue
 
         if ${STATE}; then
             echo "${FILESYSTEMS}" | grep -q "${FILESYSTEM}" || FILESYSTEMS="${FILESYSTEM};${FILESYSTEMS}"
@@ -312,9 +340,9 @@ function get_flatpak_socket() {
 
     for METADATA_FILE in "${ROOT_VAR_LIB}/flatpak/app/${APPLICATION}/current/active/metadata" \
                          "${XDG_DATA_HOME}/flatpak/app/${APPLICATION}/current/active/metadata"; do
-        [ ! -f "${METADATA_FILE}" ] && continue
+        local SOCKETS="$(get_flatpak_metadata_value "${METADATA_FILE}" 'sockets')"
 
-        local SOCKETS=$(grep '^sockets=' "${METADATA_FILE}" | awk -F'=' '{print $2}')
+        [ -z "${SOCKETS}" ] && continue
 
         if echo "${SOCKETS}" | grep -q "${SOCKET};"; then
             return 0 # True
@@ -328,17 +356,17 @@ function set_flatpak_socket() {
     local APPLICATION="${1}"
     local SOCKET="${2}"
     local STATE="${3}"
+    local CURRENT_STATE=false
+
+    get_flatpak_socket "${APPLICATION}" "${SOCKET}" && CURRENT_STATE=true
+
+    [ "${STATE}" = "${CURRENT_STATE}" ] && return
 
     for METADATA_FILE in "${ROOT_VAR_LIB}/flatpak/app/${APPLICATION}/current/active/metadata" \
                          "${XDG_DATA_HOME}/flatpak/app/${APPLICATION}/current/active/metadata"; do
-        [ ! -f "${METADATA_FILE}" ] && continue
+        local SOCKETS="$(get_flatpak_metadata_value "${METADATA_FILE}" 'sockets')"
 
-        local SOCKETS=$(grep '^sockets=' "${METADATA_FILE}" | awk -F'=' '{print $2}')
-        local CURRENT_STATE=false
-
-        get_flatpak_socket "${APPLICATION}" "${SOCKET}" && CURRENT_STATE=true
-
-        [ "${STATE}" = "${CURRENT_STATE}" ] && return
+        [ -z "${SOCKETS}" ] && continue
 
         if ${STATE}; then
             echo "${SOCKETS}" | grep -q "${SOCKET};" || SOCKETS="${SOCKET};${SOCKETS}"
