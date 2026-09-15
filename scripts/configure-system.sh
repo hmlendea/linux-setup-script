f95e3af0a7e7fe79f40bf730f2c2bd3e2ad8cb30
Horațiu Mlendea <10261133+hmlendea@users.noreply.github.com>
2026-09-15 14:37:40 +0300

    Improved VS Code settings file detection

--- scripts/configure-system.sh
+++ scripts/configure-system.sh
@@ -1209,12 +1209,12 @@ if does_bin_exist 'code' 'code-oss' 'codium' 'com.visualstudio.code'; then
     VSCODE_BIN='code'
 
     # The order is important, some might be present simultaoneously for a single package
-    if does_bin_exist 'code'; then
-        VSCODE_CONFIG_FILE="${XDG_CONFIG_HOME}/Code/User/settings.json"
-        VSCODE_BIN='code'
-    elif does_bin_exist 'code-oss'; then
+    if does_bin_exist 'code-oss'; then
         VSCODE_CONFIG_FILE="${XDG_CONFIG_HOME}/Code - OSS/User/settings.json"
         VSCODE_BIN='code-oss'
+    elif does_bin_exist 'code'; then
+        VSCODE_CONFIG_FILE="${XDG_CONFIG_HOME}/Code/User/settings.json"
+        VSCODE_BIN='code'
     elif does_bin_exist 'codium'; then
         VSCODE_CONFIG_FILE="${XDG_CONFIG_HOME}/VSCodium/User/settings.json"
         VSCODE_BIN='codium'
@@ -1223,7 +1223,7 @@ if does_bin_exist 'code' 'code-oss' 'codium' 'com.visualstudio.code'; then
         VSCODE_BIN='com.visualstudio.code'
     fi
 
-    if [ ! -f "${VSCODE_CONFIG_FILE}" ] \
+    if [ ! -f "\'${VSCODE_CONFIG_FILE}\'" ] \
     || [ -z "$(cat ${VSCODE_CONFIG_FILE})" ]; then
         create_file "${VSCODE_CONFIG_FILE}"
         printf "{}" > "${VSCODE_CONFIG_FILE}"
@@ -1238,9 +1238,11 @@ if does_bin_exist 'code' 'code-oss' 'codium' 'com.visualstudio.code'; then
     set_json_property "${VSCODE_CONFIG_FILE}" '.["window.menuBarVisibility"]' 'toggle'
     set_json_property "${VSCODE_CONFIG_FILE}" '.["window.newWindowDimensions"]' 'maximized'
     #set_json_property "${VSCODE_CONFIG_FILE}" '.["window.title"]' '${dirty}${separator}${rootName}${separator}VS Code'
-    set_json_property "${VSCODE_CONFIG_FILE}" '.["window.titleBarStyle"]' 'native'
-    set_json_property "${VSCODE_CONFIG_FILE}" '.["workbench.colorTheme"]' 'Default Dark+'
-    set_json_property "${VSCODE_CONFIG_FILE}" '.["workbench.iconTheme"]' 'seti'
+    set_config_values "${VSCODE_CONFIG_FILE}" \
+        '.["window.titleBarStyle"]' 'native' \
+        '.["workbench.colorTheme"]' 'Dark Modern' \
+        '.["workbench.preferredDarkColorTheme"]' 'Dark Modern' \
+        '.["workbench.iconTheme"]' 'vs-seti'
 
     # Editor appearance
     set_json_property "${VSCODE_CONFIG_FILE}" '.["editor.codeLens"]' false
